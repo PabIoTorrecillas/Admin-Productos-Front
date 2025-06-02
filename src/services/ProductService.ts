f77fa1de344da4d94c023a2e9dbf8d5e98680480
Torrecillas Pablo <22030092@itcelaya.edu.mx>
2025-06-02 16:05:08 -0600

    validando con zod

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,8 +1,21 @@
+import { DraftProductSchema, ProductsSchema, ProductSchema } from "../types";
+import {safeParse} from "valibot";
 
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data: ProductData) {
-    console.log(data);
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if(result.success) {
+        } else {
+            throw new Error('Datos no válidos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
